8cfbf942fa18a31659d919c7cf98d7d134e60191
billehhg <billehhg1@gmail.com>
2017-03-23 09:38:37 +0200

    added framework for sliding when player leaves screen. Waiting for graphics so this isn't just flashing rectangles

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # toveri-game
-There's not much here yet. I am learning lua with a few friends and hope to make this a long time project.
+There's not much here yet. I am learning lua with a few friends and hope to make this a long term project.
 
 Eventually this will hopefully be a game similar to Legend of Zelda with some of the game elements like those in Binding of Isaac while still making it a game of our own.
 If this sounds interesting you can check back in a few months or contact me if you like the idea and want to contribute an idea or code.
-for now, if you want to try what we have, download the lua love engine and follow the directions there for opening the game in your OS
+For now, if you want to try what we have, download the lua love engine and follow the directions there for opening the game in your OS
+
+here is a link to the lua love engine for running the game: https://love2d.org/
+
+This page describes how to run it: https://love2d.org/wiki/Getting_Started
+
+If you want to edit the code just use any text editor that supports syntax (the syntax isn't required but it helps)
+I'm using atom: https://atom.io/
+
+We'll upload a game you can play without installing love as soon as we feel there's something here
--- game/main.lua
+++ game/main.lua
@@ -1,17 +1,17 @@
 -- This happens when game is loaded
 function love.load()
---My computer reads windows in a weird way. This makes sure it fills screen
-  love.window.setMode(0,0)
-  --this makes it fullscreen
-  love.window.setFullscreen(true, "desktop")
+--  love.window.setMode(0,0)
+--love.window.setFullscreen(true, "desktop")
   --this gets the screen dimensions
   width, height = love.graphics.getDimensions()
   --this draws a rectangle in the center of the screen at (x,y) sized (w,h)
-  x, y, w, h = (width/2)-25, (height/2)-40, 50, 80
+  x, y, w, h = (width/2)-25, (height/2)-40, height/12, width/12
+  pspeed = width/6
   --current direction string. used to restrict diagonal movment without hurting functionality of x.y movment
   curdir=""
   dir="d"
   mov="s1"
+  offscreen=""
 end
 
 --this will checked if you're focused on the window
@@ -35,19 +35,86 @@ end
 
 --this function updates every second
 function love.update(dt)
+   if dt < 1/60 then
+      love.timer.sleep(1/60 - dt)
+   end
   if gameIsPaused then return end
---mov the damn thing !!!speed should become a variable
-  if curdir:sub(-1) == "w" then  y = y - 3  dir = "u" mov = mov:gsub(mov:sub(1,1),"m") end
-  if curdir:sub(-1) == "s" then  y = y + 3  dir = "d" mov = mov:gsub(mov:sub(1,1),"m") end
-  if curdir:sub(-1) == "a" then  x = x - 3  dir = "l" mov = mov:gsub(mov:sub(1,1),"m") end
-  if curdir:sub(-1) == "d" then  x = x + 3  dir = "r" mov = mov:gsub(mov:sub(1,1),"m") end
+--mov the damn thing
+  if curdir:sub(-1) == "w" then  y = y - (pspeed * dt)  dir = "u" mov = mov:gsub(mov:sub(1,1),"m") end
+  if curdir:sub(-1) == "s" then  y = y + (pspeed * dt)  dir = "d" mov = mov:gsub(mov:sub(1,1),"m") end
+  if curdir:sub(-1) == "a" then  x = x - (pspeed * dt)  dir = "l" mov = mov:gsub(mov:sub(1,1),"m") end
+  if curdir:sub(-1) == "d" then  x = x + (pspeed * dt)  dir = "r" mov = mov:gsub(mov:sub(1,1),"m") end
   if curdir == "" then  mov = "s1" end
+  --slide left when thing eaves screen
+  if offscreen == "" then
+    if x <= 0 then
+      offscreen="l"
+    end
+    if (x + w) >= width then
+      offscreen="r"
+    end
+    if y <= 0 then
+      offscreen="u"
+    end
+    if (y + h) >= height then
+      offscreen="d"
+    end
+  end
 end
 
 --this draws the screen every second
 function love.draw()
+  --this is probably fixed by setting offscreen as a two part variable
+  if offscreen == "l" then
+    num = width/20
+    offscreenx=20
+    offscreeny=0
+    offscreen= offscreen .. "y"
+  end
+  if offscreen == "r" then
+    num = width/20
+    offscreenx=-20
+    offscreeny=0
+    offscreen= offscreen .. "y"
+  end
+  if offscreen == "u" then
+    num = height/20
+    offscreenx=0
+    offscreeny=20
+    offscreen= offscreen .. "y"
+  end
+  if offscreen == "d" then
+    num = height/20
+    offscreenx=0
+    offscreeny=-20
+    offscreen= offscreen .. "y"
+  end
+  if offscreen:sub(2,2) == "y" then
+      if offscreen:sub(1,1) == "l" then offscreenx = offscreenx + 20 end
+      if offscreen:sub(1,1) == "r" then offscreenx = offscreenx - 20 end
+      if offscreen:sub(1,1) == "u" then offscreeny = offscreeny + 20 end
+      if offscreen:sub(1,1) == "d" then offscreeny = offscreeny - 20 end
+      love.graphics.translate(offscreenx,offscreeny)
+      love.timer.sleep(1/200)
+      love.graphics.setColor(224, 189, 65)
+      love.graphics.rectangle("fill", -width, 0, width, height)
+      love.graphics.setColor(189, 224, 65)
+      love.graphics.rectangle("fill", 0, -height, width, height)
+      love.graphics.setColor(65, 224, 141)
+      love.graphics.rectangle("fill", width, 0, width, height)
+      love.graphics.setColor(65, 165, 224)
+      love.graphics.rectangle("fill", 0, height, width, height)
+      love.graphics.rectangle("fill", x, y, w, h)
+      num=num-1
+      print (10 *((width/10)-num))
+      if num == 0 then
+        offscreen="done"
+      end
+  end
   wnum=tonumber(mov:sub(2,2))
   if dir == "u" then
+--love.graphics.setColor(244, 146, 66)
+--love.graphics.rectangle("fill", -width, 0, width, height)
     if mov:sub(1,1) == 'm' then
       if wnum <= 5 then
         mov = mov:gsub(wnum,wnum+1)
@@ -64,13 +131,13 @@ function love.draw()
     if mov:sub(1,1) == 'm' then
       if wnum <= 5 then
         mov = mov:gsub(wnum,wnum+1)
-        love.graphics.setColor(0, 0, 100)
+        love.graphics.setColor(0, 50, 100)
       else
         mov = mov:gsub(wnum,1)
-        love.graphics.setColor(0, 0, 50)
+        love.graphics.setColor(0, 50, 50)
       end
     else
-      love.graphics.setColor(0, 0, 200)
+      love.graphics.setColor(0, 50, 200)
     end
   end
   if dir == "l" then
